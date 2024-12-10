2bb1eb43b548a1c230804b59475561be0e1c98e4
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-12-10 17:24:35 +0100

    add show raute

--- routes/movies.js
+++ routes/movies.js
@@ -12,6 +12,22 @@ router.get('/', (req, res) => {
         })
     })
 
+
+    router.get('/:id', (req, res) => {
+
+        const id = req.params.id
+        const sql = `SELECT * FROM movies WHERE id = ?`
+
+
+        connection.query(sql, [id], (err, results) => {
+            if (err) return res.status(500).json({ err: err })
+            if (results.length == 0) return res.status(404).json({ err: 'film non trovato' })
+
+            res.json(results[0])
+        })
+
+    })
+
 })
 
 module.exports = router
